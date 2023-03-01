e5af478989737498f34dd49f8d203fa90a7ff9a9
Kevin Willian <kevinwillian084@gmail.com>
2023-02-28 22:49:40 -0300

    feat: add initial Header component with styles

---
+++ web/src/components/Header/index.tsx
@@ -0,0 +1,13 @@
+import { Logo } from './Logo';
+import { StyledContainer } from './styles';
+
+function Header() {
+  return (
+    <StyledContainer>
+      <Logo />
+      Cart
+    </StyledContainer>
+  );
+}
+
+export default Header;
--- web/src/components/Header/styles.ts
+++ web/src/components/Header/styles.ts
@@ -1,4 +1,10 @@
 import styled from 'styled-components';
+export const StyledContainer = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: 50px 0;
+`;
 export const StyledLogo = styled(Link)`
   a {
     transition: opacity 0.2s;
