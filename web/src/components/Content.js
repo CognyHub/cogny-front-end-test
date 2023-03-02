b67d7256d9e19aadf205c03ec66abfec033432b3
jefersongjr <jeferson_gjr@hotmail.com>
2023-03-02 20:49:20 -0300

    :recycle: refactor: adicionando Header

--- web/src/components/Content.js
+++ web/src/components/Content.js
@@ -1,15 +1,24 @@
+import React from 'react';
 import { BrowserRouter, Route, Switch} from 'react-router-dom';
+import Header from "./Header";
+import Cart from '../page/Cart';
 import Home from '../page/Home';
 
 function Content() {
 
 return (
   <BrowserRouter>
+  <React.StrictMode>
+      <Header />
     <Switch>
-      <Route path='/'>
+      <Route exact path='/'>
         <Home />
       </Route>
+      <Route exact path='/cart'>
+        <Cart />
+      </Route>
     </Switch>
+    </React.StrictMode>
   </BrowserRouter>
 
   );
