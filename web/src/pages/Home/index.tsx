dc79bac277d0582bea736b49b935019c5b56ef64
Kevin Willian <kevinwillian084@gmail.com>
2023-03-02 02:19:55 -0300

    refactor: add a new compact structure to the Home page for displaying products

--- web/src/pages/Home/index.tsx
+++ web/src/pages/Home/index.tsx
@@ -1,37 +1,30 @@
 import { useEffect, useState } from 'react';
+
+import Loading from '../../components/Loading';
+import { getProducts } from '../../services/dbService';
+
+import { ProductList } from '../../components/ProductList';
 import { Product } from '../../types/product';
-import staticProducts from '../../staticData.json';
-export default function Home() {
+
+const Home = (): JSX.Element => {
   const [products, setProducts] = useState<Product[]>([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchProducts = async () => {
-      const productCollection = collection(db, 'products');
-      const data = await getDocs(productCollection);
-
-      const formattedData = data.docs.map((doc) => ({
-        ...doc.data(),
-        id: doc.id,
-      })) as Product[];
-
-      setProducts(formattedData);
+      try {
+        const productList = await getProducts();
+        setProducts(productList);
+      } catch (error) {
+        console.error('Falha ao buscar produtos', error);
+      } finally {
+        setLoading(false);
+      }
     };
     fetchProducts();
   }, []);
 
-  return (
-    <div>
-      {products.map((product) => (
-        <div key={product.id}>
-          <img src={product.image} alt={product.description} />
-          <strong>{product.description}</strong>
-          <span>{product.price}</span>
-          <span>Stock{product.stock}</span>
-          <button type="button" onClick={() => {}}>
-            Add to cart
-          </button>
-        </div>
-      ))}
-    </div>
-  );
-}
+  return loading ? <Loading /> : <ProductList products={products} />;
+};
+
+export default Home;
