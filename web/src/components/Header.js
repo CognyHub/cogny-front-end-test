d5091a0f38526739d28d53da923db9fd539b4f6b
jefersongjr <jeferson_gjr@hotmail.com>
2023-03-02 20:49:50 -0300

    :sparkles: feat: criando navegação para página cart

--- web/src/components/Header.js
+++ web/src/components/Header.js
@@ -1,23 +1,29 @@
+import { useHistory } from 'react-router-dom';
 import '../style/Header.css'
 
 function Header() {
-    return (
-      <header className='header'>
-        <div className='logo-container'>
+  const navigate = useHistory();
+  return (
+    <header className='header'>
+      <div className='logo-container'>
         <h1 className='text-logo'>CognyShoes</h1>
-        <img 
-            src='https://flaticons.net/icon.php?slug_category=shopping&slug_icon=shoe'
-            alt='tenis-logo'
-            width='50px'
+        <img
+          src='https://flaticons.net/icon.php?slug_category=shopping&slug_icon=shoe'
+          alt='tenis-logo'
+          width='50px'
         />
-        </div>
-        <nav>
-        <button>Meu Carrinho</button>
+      </div>
+      <nav>
+        {/* <NavLink to="/cart">About</NavLink> */}
+
+        <button
+          onClick={() => navigate.push('/cart')}
+        >
+          Meu Carrinho</button>
         <span>0</span>
-        </nav>
-      </header>
-    );
-  }
-  
-  export default Header;
-  
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
