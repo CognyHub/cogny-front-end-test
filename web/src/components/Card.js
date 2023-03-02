563b0e9d372d542059eda3dd968061903e38cf71
jefersongjr <jeferson_gjr@hotmail.com>
2023-03-02 17:00:36 -0300

    :sparkles: feat:  criando component Card

---
+++ web/src/components/Card.js
@@ -0,0 +1,23 @@
+import '../style/CardProduct.css'
+
+function Card(img, product, description, price) {
+  return (
+   <div className='card'>
+         <img 
+            src={img} 
+            width='100%' 
+            alt={`Imagem do tenis ${product}`}
+        />
+         <p className='text-card'>{ description}</p>
+         <p className='text-card price'>{ price }</p>
+         <div className='button-container'>
+         <div className='quantity'>
+            <span>1</span>
+        </div>
+         <button className='button'>Adicionar ao Carrinho</button>
+         </div>
+      </div>
+  );
+}
+
+export default Card;
