67cbd02cbaa8a8b240031941ddc61ee81e43a7d7
jefersongjr <jeferson_gjr@hotmail.com>
2023-03-02 17:01:04 -0300

    :recycle: refactor : componetizando o elemento card

--- web/src/components/Content.js
+++ web/src/components/Content.js
@@ -1,35 +1,31 @@
 import { useEffect, useState } from 'react';
 import { collection, query, onSnapshot } from "firebase/firestore";
 import { db } from '../connection/firebaseConnection';
-import '../style/CardProduct.css'
+import '../style/Content.css'
+import Card from './Card';
 
 function Content() {
-    const[produtos, setProdutos] = useState([]);
+    const[products, setProducts] = useState([]);
 
     useEffect(() =>{
         const response = query(collection(db, 'lojaDB'));
         onSnapshot(response,(querySnapshot) => {
-            setProdutos(querySnapshot.docs.map(doc=>( {
+            setProducts(querySnapshot.docs.map(doc=>( {
                 data: doc.data()
             })))
         })
     },[]);
 
     return (
-      <div className='card'>
-         <img 
-            src={produtos[0]?.data?.image} 
-            width='100%' 
-            alt={`Imagem do tenis ${produtos[0]?.data?.product}`}
-        />
-         <p className='text-card'>{ produtos[0]?.data?.description }</p>
-         <p className='text-card price'>{ produtos[0]?.data?.price.toString().replace(".", ",") }</p>
-         <div className='button-container'>
-         <div className='quantity'>
-            <span>1</span>
-        </div>
-         <button className='button'>Adicionar ao Carrinho</button>
-         </div>
+      <div className='main'>
+
+         {products.map((product) => (
+            Card(product.data.image,
+                product.data.product,
+                product.data.description,
+                product.data.price
+                )
+         ))}
       </div>
     );
   }
