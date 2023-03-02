b5a2cee12b86ef8fc92224b3bc8f7234b9b017c3
Rafael Moraes <rafaelsm003@gmail.com>
2023-03-02 06:09:02 -0300

    feat: cria função getProductsFromFirestore, que busca itens da coleção do firestore

---
+++ web/src/firebase/getProductsFromFirestore.js
@@ -0,0 +1,18 @@
+import { getDocs } from 'firebase/firestore';
+import productsCollection from '.';
+
+const getProductsFromFirestore = async () => {
+  const products = [];
+
+  const querySnapshot = await getDocs(productsCollection);
+  querySnapshot.forEach((doc) => {
+    products.push({
+      id: doc.id,
+      ...doc.data(),
+    });
+  });
+
+  return products;
+};
+
+export default getProductsFromFirestore;
