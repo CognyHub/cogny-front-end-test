51509320ef4afb2cd9e7849b9fd17179bad4cf3d
jefersongjr <jeferson_gjr@hotmail.com>
2023-03-02 18:12:36 -0300

    :recycle: refactor:  renomeando pagina Home

---
+++ web/src/page/Home.js
@@ -0,0 +1,34 @@
+import { useEffect, useState } from 'react';
+import { collection, query, onSnapshot } from "firebase/firestore";
+import { db } from '../connection/firebaseConnection';
+import '../style/Home.css'
+import Card from '../components/Card';
+
+function Home() {
+    const[products, setProducts] = useState([]);
+
+    useEffect(() =>{
+        const response = query(collection(db, 'lojaDB'));
+        onSnapshot(response,(querySnapshot) => {
+            setProducts(querySnapshot.docs.map(doc=>( {
+                data: doc.data()
+            })))
+        })
+    },[]);
+
+    return (
+      <div className='main'>
+
+         {products.map((product) => (
+            Card(product.data.image,
+                product.data.product,
+                product.data.description,
+                product.data.price.toFixed(2).toString().replace('.', ',')
+                )
+         ))}
+      </div>
+    );
+  }
+  
+  export default Home;
+  
