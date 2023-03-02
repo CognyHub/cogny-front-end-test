09b918054e7c83d0e86ca13e156be9de889f0c4d
joao966 <joaooliveira966@gmail.com>
2023-03-02 06:52:40 -0300

    FIX: clear products seletecds

--- web/src/components/atoms/global/InfoScreen/ScreenButton/index.jsx
+++ web/src/components/atoms/global/InfoScreen/ScreenButton/index.jsx
@@ -1,5 +1,9 @@
-import * as S from './styles'
+import { useContext } from 'react';
+import * as S from './styles';
+import Context from '../../../../../context';
 
 export default function ScreenButton({ name, link }) {
-  return <S.ScreenButtonStyled to={link}>{name}</S.ScreenButtonStyled>
+  const { setProductsSelected } = useContext(Context);
+
+  return <S.ScreenButtonStyled onClick={() => setProductsSelected([])} to={link}>{name}</S.ScreenButtonStyled>;
 }
