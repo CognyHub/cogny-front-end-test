25bf462005375ceca5da718418ae841daca4cd14
Brendon <brendon.lopes.21@gmail.com>
2023-03-02 01:41:56 -0300

    feat: :sparkles: add link to Checkout page

--- web/src/components/Header/Header.jsx
+++ web/src/components/Header/Header.jsx
@@ -1,11 +1,18 @@
 import './header.css';
+import { useNavigate } from 'react-router-dom'
 
 export default function Header({ cartLength }) {
+  const navigate = useNavigate();
+
+  const onCartClick = () => {
+    navigate('/cart');
+  }
+
   return (
     <header>
       <h1>Cognyshoes</h1>
 
-      <section>
+      <section onClick={onCartClick}>
         <p>Meu carrinho</p>
         <p>{cartLength}{cartLength === 1 ? ' item' : ' itens'}</p>
       </section>
