2a949190c476615adf1def02fd93879275edff7b
Brendon <brendon.lopes.21@gmail.com>
2023-03-02 16:39:08 -0300

    feat: :sparkles: add cart component

--- web/src/components/index.jsx
+++ web/src/components/index.jsx
@@ -1,4 +1,5 @@
 import ShoeCard from './ShoeCard/ShoeCard';
 import Header from './Header/Header';
+import Cart from './Cart/Cart';
 
-export { ShoeCard, Header };
+export { ShoeCard, Header, Cart };
