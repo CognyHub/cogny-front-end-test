fe92f303c5985008e255aee95c420b2d98f361cf
Rafael Moraes <rafaelsm003@gmail.com>
2023-03-02 06:07:01 -0300

    feat: implementa componente CartItems

--- web/src/App.js
+++ web/src/App.js
@@ -1,18 +1,22 @@
 import React, { useEffect, useState } from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
+import CartItems from './components/CartItems';
 import Products from './pages/Products';
 import loadLocalStorage from './utils/loadLocalStorage';
 
 function App() {
+  const [cartItems, setCartItems] = useState(0);
+
   useEffect(() => {
     loadLocalStorage();
   }, []);
 
   return (
     <>
+      <CartItems cartItems={cartItems} />
       <Routes>
         <Route path="/" element={<Navigate to="/products" />} />
-        <Route path="/products" element={<Products />} />
+        <Route path="/products" element={<Products setCartItems={setCartItems} />} />
       </Routes>
     </>
   );
