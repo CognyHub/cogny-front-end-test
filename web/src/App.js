82b6a5cf2c9b315bb38467abb6ea07dc4cb0aebe
jefersongjr <jeferson_gjr@hotmail.com>
2023-03-02 20:48:33 -0300

    :recycle: refactor: mudando Header

--- web/src/App.js
+++ web/src/App.js
@@ -1,11 +1,9 @@
 import Content from "./components/Content";
-import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
-        <Header />
-        <Content />
+      <Content />
     </div>
   );
 }
