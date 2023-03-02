a50cac4d1938f11c95ddfa68586847869d2a31ff
Rafael Moraes <rafaelsm003@gmail.com>
2023-03-02 06:08:20 -0300

    feat: inicializa firebase e coleção no firestore

---
+++ web/src/firebase/index.js
@@ -0,0 +1,33 @@
+import { initializeApp } from 'firebase/app';
+import { collection, getFirestore } from 'firebase/firestore';
+import { envs } from '../envs';
+
+// DOTENV não estava funcionando 
+// Para configurar o Firestore, crie o arquivo envs.js 
+// e coloque as variáveis nele, seguindo a estrutura abaixo:
+
+const {
+  REACT_APP_API_KEY,
+  REACT_APP_AUTH_DOMAIN,
+  REACT_APP_PROJECT_ID,
+  REACT_APP_STORAGE_BUCKET,
+  REACT_APP_MESSAGING_SENDER_ID,
+  REACT_APP_APP_ID,
+} = envs;
+
+const firebaseConfig = {
+  apiKey: REACT_APP_API_KEY,
+  authDomain: REACT_APP_AUTH_DOMAIN,
+  projectId: REACT_APP_PROJECT_ID,
+  storageBucket: REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: REACT_APP_MESSAGING_SENDER_ID,
+  appId: REACT_APP_APP_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+
+const firestore = getFirestore(app);
+
+const productsCollection = collection(firestore, 'products');
+
+export default productsCollection;
