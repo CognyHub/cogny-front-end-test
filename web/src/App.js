4fb714a030490e74bb97e4c810fdbdebea9bc590
Rafael Moraes <rafaelsm003@gmail.com>
2023-03-02 06:59:25 -0300

    feat: implementa a rota /cart

--- web/src/App.js
+++ web/src/App.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import CartItems from './components/CartItems';
 import Products from './pages/Products';
+import Cart from './pages/Cart';
 import loadLocalStorage from './utils/loadLocalStorage';
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Navigate to="/products" />} />
         <Route path="/products" element={<Products setCartItems={setCartItems} />} />
+        <Route path="/cart" element={<Cart setCartItems={setCartItems} />} />
       </Routes>
     </>
   );
