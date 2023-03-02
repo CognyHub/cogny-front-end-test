e0f506b11d09d9485b456f76b53e9b786e573d55
Kevin Willian <kevinwillian084@gmail.com>
2023-03-02 02:24:06 -0300

    feat: add initial view of Cart page

--- web/src/pages/Cart/index.tsx
+++ web/src/pages/Cart/index.tsx
@@ -1,3 +1,16 @@
+import { useCart } from '../../context/CartProvider';
+
 export default function Cart() {
-  return <>CartPage</>;
+  const { cart } = useCart();
+  return (
+    <div>
+      {cart.map((product) => (
+        <div key={product.id}>
+          <img src={product.image} alt={product.description} />
+          <strong>{product.description}</strong>
+          <span>{product.price}</span>
+        </div>
+      ))}
+    </div>
+  );
 }
