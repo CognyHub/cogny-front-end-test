d628bb9b8e0fbbfbd0faf6a03d3615c618edc39d
Rafael Moraes <rafaelsm003@gmail.com>
2023-03-02 06:12:14 -0300

    feat: cria componente ProductCard, que renderiza card de cada produto

---
+++ web/src/components/ProductCard.js
@@ -0,0 +1,9 @@
+import React, { useEffect, useState } from 'react';
+
+function ProductCard({ id, title, price, imgUrl, setCartItems }) {
+  return (
+    <h1>card</h1>
+  );
+}
+
+export default ProductCard;
