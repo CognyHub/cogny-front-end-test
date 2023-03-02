6ab668c3718d0c9c6d861df460b04a867ad76a5d
Brendon <brendon.lopes.21@gmail.com>
2023-03-02 01:26:45 -0300

    refactor: :recycle: lift states
    to refresh all desired components when relevant states are updated

--- web/src/pages/Home.jsx
+++ web/src/pages/Home.jsx
@@ -1,21 +1,44 @@
 import { useEffect, useState } from 'react'
-import { ShoeCard } from '../components';
-import { getShoes } from '../services/firebase/shoes.services';
+import { Header, ShoeCard } from '../components';
+import * as shoesServices from '../services/firebase/shoes.services';
+import * as cartServices from '../services/firebase/cart.services';
 
 export default function Home() {
   const [shoes, setShoes] = useState([]);
+  const [cartLength, setCartLength] = useState(0);
+  const [addingToCart, setAddingToCart] = useState(false);
+
+  const addShoeToCart = async (id) => {
+    console.log('Adicionando ao carrinho: ' + id);
+    setAddingToCart(true);
+
+    await cartServices.addToCart(id);
+
+    setAddingToCart(false);
+  }
 
   useEffect(() => {
     document.title = 'Cognyshoes - Home';
-    getShoes().then((shoes) => setShoes(shoes))
-  }, []);
+    shoesServices.getShoes()
+      .then((shoes) => setShoes(shoes));
+
+    cartServices.getCart()
+      .then((cart) => setCartLength(cart.length));
+  }, [addingToCart]);
 
   return (
     <div>
-      Home
+      <Header cartLength={cartLength} />
       {shoes.length === 0
         ? <p>Loading...</p>
-        : shoes.map((shoe) => <ShoeCard key={shoe.id} {...shoe} />) }
+        : shoes.map((shoe) => (
+          <ShoeCard
+            key={shoe.id}
+            addShoeToCart={addShoeToCart}
+            addingToCart={addingToCart}
+            {...shoe}
+          />)
+        ) }
     </div>
   )
 }
