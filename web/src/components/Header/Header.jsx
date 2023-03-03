a93a36cd9f08b519c99491b37d1429480ada6433
Brendon <brendon.lopes.21@gmail.com>
2023-03-02 23:56:30 -0300

    style: :lipstick: update Header CSS

--- web/src/components/Header/Header.jsx
+++ web/src/components/Header/Header.jsx
@@ -14,14 +14,24 @@ export default function Header({ cartLength }) {
 
   return (
     <header>
-      <div onClick={onLogoClick}>
-        <h1>Cognyshoes</h1>
+      <div onClick={onLogoClick} className="logo-name-wrapper">
+        <h1>COGNYSHOES</h1>
         <img src="/logo.png" alt="Cognyshoes" />
       </div>
 
-      <section onClick={onCartClick}>
-        <p>Meu carrinho</p>
-        <p>{cartLength}{cartLength === 1 ? ' item' : ' itens'}</p>
+      <section onClick={onCartClick} className="cart-link">
+        <div>
+          <p className="cart-link-text">Meu carrinho</p>
+          <p className="cart-link-number">
+            {cartLength}{cartLength === 1 ? ' item' : ' itens'}
+          </p>
+        </div>
+
+        <img
+          className="cart-icon"
+          src="/cart-icon.jpg"
+          alt="carrinho de compras"
+        />
       </section>
     </header>
   )
