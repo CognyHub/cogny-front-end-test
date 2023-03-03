d51016c0de9b3b42e5f0e6ea7eb31ce48d4ff233
Brendon <brendon.lopes.21@gmail.com>
2023-03-02 23:16:13 -0300

    feat: :sparkles: implement Cart component

--- web/src/components/Cart/Cart.jsx
+++ web/src/components/Cart/Cart.jsx
@@ -1,7 +1,32 @@
-export default function Cart() {
+import { ShoeCheckoutCard } from '../';
+import * as checkoutServices from '../../services/firebase/checkout.services';
+import { transformPrice } from '../../utils/transformPrice';
+
+export default function Cart({ shoes, ids, setCart }) {
+  const finishOrderOnClick = () => {
+    checkoutServices.finishOrder(ids);
+    setCart([]);
+    alert('Pedido finalizado com sucesso!');
+  }
+
   return (
     <div>
-      Cart
+      {shoes.map((shoe) => <ShoeCheckoutCard key={shoe.description} {...shoe} />)}
+      <button
+        className="finishe-order-btn"
+        onClick={finishOrderOnClick}
+      >
+          FINALIZAR PEDIDO
+      </button>
+
+      <section className="total-price-wrapper">
+        <span>
+          TOTAL
+        </span>
+        <span>
+          {transformPrice(shoes.reduce((acc, curr) => acc + curr.price, 0))}
+        </span>
+      </section>
     </div>
   );
-}
+};
