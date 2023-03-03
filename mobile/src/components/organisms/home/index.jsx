9aa1c0d99bc8242f867a2b56803631b35d5bce20
joao966 <joaooliveira966@gmail.com>
2023-03-03 05:07:02 -0300

    FIX: add loading

--- mobile/src/components/organisms/home/index.jsx
+++ mobile/src/components/organisms/home/index.jsx
@@ -2,36 +2,38 @@ import Card from '../../molecules/home/Card';
 import * as S from './styles';
 import { useContext } from 'react';
 import Context from '../../../context';
-import { FlatList } from 'react-native';
+import { FlatList, Text } from 'react-native';
 import Menu from '../global/Menu';
 
 export default function HomeOrganisms() {
-  const { products, productsSelected, setProductsSelected } = useContext(Context);
+  const { products, productsSelected, setProductsSelected, loading } = useContext(Context);
 
   return (
     <S.Container>
       <Menu />
       <S.Wrapper>
-      <FlatList
-        data={products}
-        renderItem={({ item: { id, imageUrl, description, price } }) => {
-          return (
-            <Card
-              link={imageUrl}
-              title={description}
-              price={price}
-              handleSelect={() =>
-                setProductsSelected([...productsSelected, id])
-              }
-              selected={
-                productsSelected.filter((el) => el === id).length !== 0
-              }
-              qtd={productsSelected.filter((el) => el === id).length}
-            />
-          )
-        }}
-        keyExtractor={item => item.id}
-      />
+      {!loading ? (
+        <FlatList
+            data={products}
+            renderItem={({ item: { id, imageUrl, description, price } }) => {
+              return (
+                <Card
+                  link={imageUrl}
+                  title={description}
+                  price={price}
+                  handleSelect={() =>
+                    setProductsSelected([...productsSelected, id])
+                  }
+                  selected={
+                    productsSelected.filter((el) => el === id).length !== 0
+                  }
+                  qtd={productsSelected.filter((el) => el === id).length}
+                />
+              )
+            }}
+            keyExtractor={item => item.id}
+          />
+      ) : <Text>Loading...</Text>}
       </S.Wrapper>
     </S.Container>
   )
--- mobile/src/context/Provider.jsx
+++ mobile/src/context/Provider.jsx
@@ -5,18 +5,32 @@ import Context from './';
 const Provider = ({ children }) => {
   const [productsSelected, setProductsSelected] = useState([]);
   const [products, setPorducts] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
+    setLoading(true);
     getProducts()
       .then(({docs}) => {
         setPorducts(docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+        setLoading(false);
       })
-      .catch((error) => console.error(error));
+      .catch((error) => {
+        console.error(error);
+        setLoading(true);
+      });
   }, []);
   
 
   return (
-    <Context.Provider value={{ products, productsSelected, setProductsSelected }}>
+    <Context.Provider
+      value={{
+        products,
+        productsSelected,
+        setProductsSelected,
+        loading,
+        setLoading
+      }}
+    >
       {children}
     </Context.Provider>
   );
