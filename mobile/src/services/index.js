6661cfb38c051ad43ab2e1c6ad8821ee6b8493e8
joao966 <joaooliveira966@gmail.com>
2023-03-03 05:22:08 -0300

    ADD: service on firebase

---
+++ mobile/src/services/index.js
@@ -0,0 +1,30 @@
+import { collection, getDocs, getFirestore } from "@firebase/firestore";
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket:process.env.REACT_APP_STORANGE,
+  messagingSenderId: process.env.REACT_APP_MESSA,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MENSURE
+};
+
+
+const app = initializeApp(firebaseConfig);
+
+export function getProducts() {
+  return new Promise((resolve, reject) => {
+    try {
+      const db = getFirestore(app);
+      const collectionRef = collection(db, 'products');
+      const data = getDocs(collectionRef);
+      resolve(data);
+      return;
+    } catch (error) {
+      reject(new Error('Erro ao trazer produtos!'));
+      return;
+    }
+  });
+}
