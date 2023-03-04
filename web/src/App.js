6b6d78edb55b48547ad7de477eefda8d01afd37d
André Santos <x_sode@hotmail.com>
2023-03-04 00:17:44 -0300

    'feat: access the database'

--- web/src/App.js
+++ web/src/App.js
@@ -1,8 +1,32 @@
 import './App.css';
+import { initializeApp } from 'firebase/app';
+import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAzQ6afC5bXqK79p6N5YcIiYFjeoAI1kDo",
+  authDomain: "cogny-front-end-test-b0e85.firebaseapp.com",
+  projectId: "cogny-front-end-test-b0e85",
+  storageBucket: "cogny-front-end-test-b0e85.appspot.com",
+  messagingSenderId: "922557791872",
+  appId: "1:922557791872:web:4374a677f1e3985b1af065",
+  measurementId: "G-F9TSS3DBR8"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+const getShoes = async (db) => {
+  const shoesCol = collection(db, 'shoes');
+  const shoesSnapshot = await getDocs(shoesCol);
+  const shoesList = shoesSnapshot.docs.map(doc => doc.data());
+  return shoesList;
+}
+
+const 
 
 function App() {
   return (
-    <h>hi</h>
+    <h1>{console.log(getShoes(db))}</h1>
   );
 }
 
