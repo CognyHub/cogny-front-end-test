10058297f6682519b07d631feb0ceacfb117abc6
jefersongjr <jeferson_gjr@hotmail.com>
2023-03-04 12:28:51 -0300

    :recycle: refactor:adicionando classes

--- web/src/components/Card.js
+++ web/src/components/Card.js
@@ -3,16 +3,18 @@ import '../style/CardProduct.css'
 function Card(img, product, description, price, click, key) {
   return (
    <div className='card' key={key}>
-         <img 
+         <img
+            className='product-image'
             src={img} 
-            width='100%' 
             alt={`Imagem do tenis ${product}`}
         />
-         <p className='text-card'>{ description}</p>
-         <p className='text-card price'>{ price }</p>
+        <div className='text-container'>
+           <p className='text-card'>{ description}</p>
+           <p className='text-card price'>{ price }</p>
+        </div>
          <div className='button-container'>
          <div className='quantity'>
-            <span>1</span>
+            <span className='quantity-number'>1</span>
         </div>
          <button className='button' onClick={click}>Adicionar ao Carrinho</button>
          </div>
