f84ba184c43c44bf573cead6aab18f6f9aedfe3a
jefersongjr <jeferson_gjr@hotmail.com>
2023-03-04 14:50:59 -0300

    :recycle: refactor: trocando '.' por ','

--- web/src/components/CartCard.js
+++ web/src/components/CartCard.js
@@ -1,38 +1,52 @@
-import { useState, useContext,useEffect } from 'react';
+import { useState, useContext, useEffect } from 'react';
 import CartContext from '../context/CartContext';
+import '../style/CardCart.css'
+
 
 function CartCard(img, product, description, price, quant, key) {
-    const [ quantity,setQuantity ] = useState(quant || 0);
-     const { setTotal } = useContext(CartContext);
-     const { orders } = useContext(CartContext);
+  const [quantity, setQuantity] = useState(quant || 0);
+  const { setTotal } = useContext(CartContext);
+  const { orders } = useContext(CartContext);
 
-     useEffect(() =>{
-      const totalPrice = orders.reduce((total, valor) => total + valor.price,0);
-      setTotal( totalPrice);
-    },[orders, setTotal]);
+  useEffect(() => {
+    const totalPrice = orders.reduce((total, valor) => total + valor.price, 0);
+    setTotal(totalPrice);
+  }, [orders, setTotal]);
 
-    const handleChange = (value) => {
-          setQuantity(value)
-          setTotal((old) => old += price)
-    }
+  const handleChange = (value) => {
+    setQuantity(value)
+    setTotal((old) => old += price)
+  }
 
   return (
-   <div className='cart-card'  key={key}>
-         <img 
-            src={img} 
-            width='30px' 
-            alt={`Imagem do tenis ${product}`}
+    <div className='cart-card' key={key}>
+      <div className='left-container'>
+        <img
+          className='image-cart'
+          src={img}
+          alt={`Imagem do tenis ${product}`}
         />
-        <p>{ description } </p>
-        <p>{ price }</p>
-        <input
-            type='number' 
-            min='1'
-            value= {quantity}
-            onChange={(e) => handleChange(e.target.value, price)}
-            />
-        <p className='total-price'>{ quantity * price }</p>
+        <div className='text-cart'>
+          <p>{description} </p>
+          <p className='price-cart'>
+            {`R$${price?.toFixed(2).toString().replace('.', ',')}`}
+            </p>
+        </div>
       </div>
+      <div className='right-container'>
+      <input
+        className='input-cart'
+        type='number'
+        min='1'
+        value={quantity}
+        onChange={(e) => handleChange(e.target.value, price)}
+      />
+      <p className='total-price'>
+        {`R$${(quantity * price)?.toFixed(2).toString().replace('.', ',')}`}
+      </p>
+
+      </div>
+    </div>
   );
 }
 
--- web/src/page/Cart.js
+++ web/src/page/Cart.js
@@ -1,38 +1,51 @@
 import { useContext } from 'react';
 import CartCard from '../components/CartCard';
 import CartContext from '../context/CartContext';
+import '../style/Cart.css'
 
 function Cart() {
-  const { orders,setOrders } = useContext(CartContext);
+  const { orders, setOrders } = useContext(CartContext);
   const { total, setTotal } = useContext(CartContext);
 
   const handleClick = () => {
-    if(orders.length >= 1) {
+    if (orders.length >= 1) {
       alert("Compra Finalizada");
       setOrders([]);
       setTotal();
     }
-   }
+  }
 
   return (
-   <section className='cart-container'>
-      {orders?.map((x)=> (
+    <section className='cart-container'>
+      <div className='description'>
+        <p>Produto</p>
+          <div className='description-right'>
+            <p>Quantidade</p>
+            <p>Preço</p>        
+          </div>
+      </div>
+      {orders?.map((x) => (
         CartCard(x.image,
           x.product,
           x.description,
           x.price,
           x.quantity,
           x.product
-          ) 
+        )
       )
       )}
-      <p>{total}</p>
-      <button
-        onClick={() => handleClick()}
-      >
-        Finalizar Compra
-      </button>
-   </section>
+      <div className='finish-cart'>
+        <button
+          className='button-cart'
+          onClick={() => handleClick()}
+          >
+          Finalizar Compra
+        </button>
+        <p className='finish-total-price'>
+          {total && total.toFixed(2).toString().replace('.', ',')}
+          </p>
+      </div>
+    </section>
   );
 }
 
