6271299e3d5581400111992a25b4b28ef0aa963c
Kevin Willian <kevinwillian084@gmail.com>
2023-03-04 02:22:30 -0300

    feat: add styles for Cart component

--- web/src/pages/Cart/styles.ts
+++ web/src/pages/Cart/styles.ts
@@ -1,1 +1,28 @@
-export const a = 'a';
+import styled from 'styled-components';
+
+export const StyledMain = styled.main`
+  background: #fff;
+  border-radius: 4px;
+  padding: 30px;
+`;
+
+export const StyledSuccessPurchase = styled(StyledMain)`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  color: #f8375d;
+  height: 50vh;
+
+  h1 {
+    font-size: 36px;
+    font-weight: bold;
+    margin-bottom: 10px;
+  }
+
+  span {
+    font-size: 24px;
+    font-weight: bold;
+    margin-bottom: 10px;
+  }
+`;
