6f49fe6fe147795474d9170c628d54eb14e25318
jefersongjr <jeferson_gjr@hotmail.com>
2023-03-04 23:13:36 -0300

    :sparkles: feat: Criando conexão com firebase

---
+++ mobile/src/connection/firebaseConnection.js
@@ -0,0 +1,25 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { 
+         REACT_APP_API_KEY,
+         REACT_APP_API_KEY, 
+         REACT_APP_PROJECT_ID 
+        } from 'react-native-dotenv';
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: REACT_APP_API_KEY,
+  authDomain: REACT_APP_API_KEY,
+  projectId: REACT_APP_PROJECT_ID,
+};
+
+// Initialize Firebase
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+export {db}
