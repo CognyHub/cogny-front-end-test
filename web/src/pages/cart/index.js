eeae108d84d212f0bda270339f5ab45aa0c07414
Bruno Monteiro <brunomourabastos@gmail.com>
2023-03-04 22:34:37 -0300

    feat: Adicionado valor total do carrinho, botão de finalizar pedido e alterado tamanho da imagem para 100px

--- web/src/pages/cart/index.js
+++ web/src/pages/cart/index.js
@@ -2,21 +2,26 @@ import React, { useContext } from 'react';
 import Navbar from '../../components/navbar';
 import CartContext from '../../context/context'
 
-function Cart () {
+function Cart() {
   const { cart } = useContext(CartContext)
   return (
     <div>
       <Navbar />
       {cart.length === 0 ? <h1>Seu carrinho está vazio</h1> :
-      <div>
-      {cart.map((item, index) => (
-        <div key={index}>
-        <img width="260" src={item.image} alt={item.description} />
-        <div>{item.description}</div>
-          <p>R$ {item.price.toFixed(2).replace('.',',')}</p>
+        <div>
+          {cart.map((item, index) => (
+            <div key={index}>
+              <img width="100" src={item.image} alt={item.description} />
+              <div>{item.description}</div>
+              <p>R$ {item.price.toFixed(2).replace('.', ',')}</p>
+              <hr />
+            </div>
+          ))}
+          <div>
+            <button>Finalizar compra</button>
+            <p>TOTAL R$ {cart.reduce((acc, item) => acc + item.price, 0).toFixed(2).replace('.', ',')}</p>
+          </div>
         </div>
-      ))}
-      </div>
       }
     </div>
   )
