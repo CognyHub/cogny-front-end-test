54b2dabd559eeb96e0f9d388a62fd2af60f3a614
André Santos <x_sode@hotmail.com>
2023-03-04 23:58:52 -0300

    'chore: Cart page without contents'

---
+++ web/src/pages/Cart.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from '../componentes/Header';
+
+function App() {
+  // const [shoesList, setShoesList] = useState('')
+
+  // useEffect(()=>{
+  //   getShoes(db)
+  // }, [])
+
+  return (
+    <Header />
+  );
+}
+
+export default App;
