d430643fb49bf59e387e6762e5d731f76dc0e2f5
jefersongjr <jeferson_gjr@hotmail.com>
2023-03-05 14:55:25 -0300

    :sparkles: feat: Recebendo dados do FireBase e renderizando na tela

--- mobile/src/pages/Home.js
+++ mobile/src/pages/Home.js
@@ -1,10 +1,43 @@
-import React from 'react';
-import { View, Text } from 'react-native'
+import React, { useState, useEffect } from 'react';
+import { View, StyleSheet, ScrollView  } from 'react-native';
+import { db } from '../connection/firebaseConnection';
+import { collection, query, onSnapshot,  } from "firebase/firestore";
+import Card from '../components/Card';
 
 export default function Home() {
+  const [products, setProducts] = useState([]);
+  
+  useEffect(() =>{
+    const response = query(collection(db, 'lojaDB'));
+    onSnapshot(response,(querySnapshot) => {
+        setProducts(querySnapshot.docs.map(doc=>( {
+            data: doc.data()
+        })));
+    });
+},[]);
+
     return (
-      <View>
-        <Text>Página Home</Text>  
+      <ScrollView style={styles.main}>
+      <View style={styles.home}>
+        { products.map((x)=> (
+          Card(
+            x?.data?.image,
+            x?.data?.description,
+            x?.data?.price
+            ))
+        )} 
       </View>
+      </ScrollView>
     );
   }
+
+  const styles = StyleSheet.create({
+    home: {
+      alignItems: 'center',
+      backgroundColor: '#2E2E2E',
+      display: 'flex',
+    },
+    main: {
+      width: '100%'
+    }
+});
