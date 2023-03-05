a7efe821a39e60f6696baf38d18cff84d8541ee5
jefersongjr <jeferson_gjr@hotmail.com>
2023-03-05 14:54:54 -0300

    :bug: fix - consertando bug no Header

--- mobile/src/components/Header.js
+++ mobile/src/components/Header.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
-// import { REACT_APP_API_KEY } from 'react-native-dotenv';
-
 
 export default function Header() {
     return (
         <View style={styles.header}>
             <View style={styles.logoContainer}>
-                <Text style={styles.headerText}>{REACT_APP_API_KEY}</Text>
+                <Text style={styles.headerText}>CognyShoes</Text>
                 <Image
                     source={require('../assets/icon.png')}
-                    style={{ width: 30 }}
+                    style={{ width: 30 , height: 30}}
                 />
             </View>
             <View>
@@ -27,7 +25,6 @@ const styles = StyleSheet.create({
         backgroundColor: '#2E2E2E',
         flexDirection: 'row',
         justifyContent: 'space-evenly',
-        paddingTop: 30,
         width: '100%',
     },
     headerText: {
@@ -36,5 +33,8 @@ const styles = StyleSheet.create({
         color: '#FFF',
         letterSpacing: 1
     },
+    logoContainer: {
+        flexDirection: 'row',
+    }
 });
 
