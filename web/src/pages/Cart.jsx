e82631b2f9822e99fb4eda62627b9bcf20d4eb22
André Santos <x_sode@hotmail.com>
2023-03-05 03:33:30 -0300

    'chore: Cart page implementation'

--- web/src/pages/Cart.jsx
+++ web/src/pages/Cart.jsx
@@ -1,15 +1,50 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from '../componentes/Header';
+import ProductRow from '../componentes/ProductRow';
+import { BodyStyle, TableStyle } from '../styles';
+import { getStorageCar, sumProducts } from '../utils/fuctions';
 
 function App() {
-  // const [shoesList, setShoesList] = useState('')
-
-  // useEffect(()=>{
-  //   getShoes(db)
-  // }, [])
+  const [shoesList, setShoesList] = useState('');
+  const [sumPrice, setSumPrice] = useState('');
+  console.log(shoesList);
+  useEffect(() => {
+    setShoesList(getStorageCar());
+    setSumPrice(sumProducts());
+  }, []);
 
   return (
-    <Header />
+    <BodyStyle>
+      <Header />
+      <TableStyle>
+        <div className="tableContainer">
+          <table style={{ padding: '40px' }}>
+            <thead style={{ height: '40px', margin: '100px' }}>
+              <tr>
+                <th style={{ position: 'absolute', marginLeft: '100px' }}>PRODUTO</th>
+                <th style={{ position: 'absolute', marginLeft: '440px' }}>QTD</th>
+                <th style={{ position: 'absolute', marginLeft: '520px' }}>PREÇO</th>
+              </tr>
+            </thead>
+            <tbody>
+              {
+                        shoesList && shoesList.map((product) => (
+                          <ProductRow key={product.id} product={product} />
+                        ))
+              }
+            </tbody>
+          </table>
+          <div className="closeCart">
+            <button type="button">FINALIZAR PEDIDO</button>
+            <div className="totalCart">
+              <h3>Total</h3>
+              <h1>{sumPrice.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })}</h1>
+            </div>
+          </div>
+        </div>
+      </TableStyle>
+    </BodyStyle>
+
   );
 }
 
