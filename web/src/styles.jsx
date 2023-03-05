35fbc90df72afd6de61fc9da4bdb6825600c6c0e
André Santos <x_sode@hotmail.com>
2023-03-04 23:55:09 -0300

    'style: file of style-component'

---
+++ web/src/styles.jsx
@@ -0,0 +1,149 @@
+import styled from 'styled-components';
+
+export const BodyStyle = styled.section`
+  position: relative;
+  width: 100vmax;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: #000000;
+
+.divContainer{
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+  gap:20px;
+  padding-left:150px;
+  padding-right:150px;
+  padding: 150px;
+}
+
+`;
+
+export const HeaderStyles = styled.header`
+width: 100vmax;
+height: 15%;
+display: flex;
+position: fixed;
+top: 0;
+flex-direction: row;
+justify-content: space-between;
+align-items: center;
+color: white;
+background-color:#000000;
+padding: 40px;
+z-index:4;
+
+.carQuantity{
+  color: #A3A3A6;
+  font-weight: lighter;
+  padding-left: 45px; 
+}
+
+
+
+.logoImg{
+height: 40px;
+}
+`;
+
+export const CardStyle = styled.div`
+display: flex;
+flex-wrap: wrap;
+background-color: #FAFAFA;
+align-items: center;
+justify-content: center;
+background-color: #000000;
+
+
+.card{
+  width: 300px;
+  height:450px;
+  gap: 1em;
+  box-shadow: rgba(0, 0, 0, 0.15) 0px 2px 8px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-around;
+  border-radius: 3px;
+  background-color: white;
+  position: relative;
+  padding: 20px;
+
+
+}
+img{
+  height: 100%;
+  align-self:center;
+}
+.cardImg{
+  width: 240px;
+  background-color: rgb(255, 255, 255);
+  border-radius: 10px;
+  display: flex;
+  justify-content: center;
+  margin-top: 1em;
+  
+}
+.divButtons{
+  display: flex;
+  position: absolute;
+  bottom: 15px;
+  left: 20px;
+  align-items: center;
+  justify-content: space-between;
+  background-color:#F8375D;
+  width:250px;
+  border-radius:5px;
+  padding-right: 20px;
+
+}
+input{
+    width: 40px;
+    height:25px;
+    background: rgb(255, 255, 255);
+    border: 1px solid rgb(204, 204, 204);
+    border-radius: 8px;
+    color: rgb(51, 51, 51);
+    font-family: Roboto;
+    font-weight: lighter;
+    font-size: 16px;
+    line-height: 20px;
+    padding: 10px;
+    display: flex;
+    box-shadow: rgba(0, 0, 0, 0.15) 0px 2px 8px;
+}
+
+  button{
+    color: white;
+    background-color:#F8375D;
+    border: none;
+    height: 40px;
+    border-radius: 5px;
+    font-weight: bold;
+
+
+  }
+
+  .number{
+    background-color: #C62C4A;
+    height: 40px;
+    width: 40px;
+    font-weight: lighter;
+    color: white;
+    padding-top:10px;
+    text-align: center;
+    border-radius: 5px 0 0 5px;
+  }
+
+  .price{
+    margin-bottom:70px;
+    margin-top:30px;
+  }
+  p{
+    font-weight: 500;
+  }
+
+`;
