f9ea5b2c1c751d5425bae6ff71f375d646bbaf70
André Santos <x_sode@hotmail.com>
2023-03-04 23:54:44 -0300

    'fix: changing extention of files from .js to .jsx'

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,16 +1,15 @@
-import './App.css';
 import { Route, Routes } from 'react-router-dom';
-import Home from './pages/Home'
-import Cart from './pages/Cart'
+import React from 'react';
+import Home from './pages/Home';
+import Cart from './pages/Cart';
 import Provider from './context/Provider';
 
 function App() {
-
   return (
     <Provider>
       <Routes>
-        <Route exact path="/" element={ <Home />}/>
-        <Route exact path="/cart" element={ <Cart />}/>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/cart" element={<Cart />} />
       </Routes>
     </Provider>
   );
