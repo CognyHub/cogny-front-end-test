a2e0e2ccb8ba13bf7a64a44b8df1a29fd4ac33b6
André Santos <x_sode@hotmail.com>
2023-03-04 23:41:37 -0300

    'fix: firebaseConnection.js file'

--- web/src/firebaseConnection.js
+++ web/src/firebaseConnection.js
@@ -1,5 +1,5 @@
 // Import the functions you need from the SDKs you need
-import firebase from "firebase/app";
+import firebase from 'firebase/app';
 import 'firebase/database';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -7,17 +7,17 @@ import 'firebase/database';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAzQ6afC5bXqK79p6N5YcIiYFjeoAI1kDo",
-  authDomain: "cogny-front-end-test-b0e85.firebaseapp.com",
-  projectId: "cogny-front-end-test-b0e85",
-  storageBucket: "cogny-front-end-test-b0e85.appspot.com",
-  messagingSenderId: "922557791872",
-  appId: "1:922557791872:web:4374a677f1e3985b1af065",
-  measurementId: "G-F9TSS3DBR8"
+  apiKey: 'AIzaSyAzQ6afC5bXqK79p6N5YcIiYFjeoAI1kDo',
+  authDomain: 'cogny-front-end-test-b0e85.firebaseapp.com',
+  projectId: 'cogny-front-end-test-b0e85',
+  storageBucket: 'cogny-front-end-test-b0e85.appspot.com',
+  messagingSenderId: '922557791872',
+  appId: '1:922557791872:web:4374a677f1e3985b1af065',
+  measurementId: 'G-F9TSS3DBR8',
 };
 
 // Initialize Firebase
 
-firebase.initializeApp(firebaseConfig)
+firebase.initializeApp(firebaseConfig);
 
 export default firebase;
