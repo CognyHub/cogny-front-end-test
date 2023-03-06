fcdb7c926e756921b0c4e77cc5ac16620fce7e0a
Kevin Willian <kevinwillian084@gmail.com>
2023-03-05 22:44:42 -0300

    feat: add dbServices to handle connection with FIrebase

---
+++ mobile/src/service/dbService.ts
@@ -0,0 +1,40 @@
+import { getDocs, increment, updateDoc } from 'firebase/firestore';
+import { collection, doc, getDoc } from 'firebase/firestore';
+
+import { db } from './firebase';
+
+import { ProductCart } from './../@types/product';
+import { Product } from '../@types/product';
+import { Stock } from '../@types/stock';
+
+export const getProductById = async (productId: string) => {
+  const productRef = doc(db, 'products', productId);
+  const productSnap = await getDoc(productRef);
+  const productWithId = { id: productSnap.id, ...productSnap.data() };
+  return productWithId as Product;
+};
+
+export const getProducts = async () => {
+  const productsRef = collection(db, 'products');
+  const productsSnap = await getDocs(productsRef);
+  const productsWithId = productsSnap.docs.map((doc) => ({
+    id: doc.id,
+    ...doc.data(),
+  }));
+  return productsWithId as Product[];
+};
+
+type ChangeStockQuantityParams = {
+  stockId: Stock['id'];
+  quantity: ProductCart['quantity'];
+};
+
+export const changeStockQuantity = async ({
+  stockId,
+  quantity,
+}: ChangeStockQuantityParams) => {
+  const stockRef = doc(db, 'stock', stockId);
+  updateDoc(stockRef, {
+    quantity: increment(-quantity),
+  });
+};
