9ca220b5d74e14e0dfe576157764bb61f7a82df8
Kevin Willian <kevinwillian084@gmail.com>
2023-03-05 22:37:56 -0300

    feat: add cart types

---
+++ mobile/src/@types/cart.ts
@@ -0,0 +1,12 @@
+import { ReactNode } from 'react';
+import { ProductCart } from './product';
+
+export type CartProviderProps = {
+  children: ReactNode;
+};
+
+export type CartContextData = {
+  cart: ProductCart[];
+  addProduct: (productId: string) => Promise<void>;
+  setCart: (cart: ProductCart[]) => void;
+};
