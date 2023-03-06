3ea857b5ee1a48e4e443c87fc86d6289333b549a
Bruno Monteiro <brunomourabastos@gmail.com>
2023-03-05 23:49:29 -0300

    feat: Adicionado alerta para pedido finalizado com sucesso e limpando carrinho. Adicionado estilo

--- web/src/pages/cart/index.js
+++ web/src/pages/cart/index.js
@@ -1,42 +1,53 @@
 import React, { useContext } from 'react';
 import Navbar from '../../components/navbar';
-import CartContext from '../../context/context'
+import CartContext from '../../context/context';
+import './cart.css'
 
 function Cart() {
-  const { cart } = useContext(CartContext)
+  const { cart, setCart } = useContext(CartContext)
+
+  function finishOrder() {
+    alert('Pedido finalizado com sucesso!')
+    setCart([])
+  }
+  
   return (
-    <div>
+    <div className='cart-body'>
       <Navbar />
       {cart.length === 0 ? <h1>Seu carrinho está vazio</h1> :
-        <table>
-          <thead>
-            <tr>
-              <th></th>
-              <th>PRODUTO</th>
-              <th>QTD</th>
-              <th>PREÇO</th>
-            </tr>
-          </thead>
-          <tbody>
-            {cart.map((item, index) => (
-              <tr key={index}>
-                <td><img width="100" src={item.image} alt={item.description} /></td>
-                <td>{item.description}</td>
-                <td>{item.quantity}</td>
-                <td>R$ {item.price.toFixed(2).replace('.', ',')}</td>
+        <div className='cart-table'>
+          <table >
+            <thead>
+              <tr className='table-head-row'>
+                <th></th>
+                <th>PRODUTO</th>
+                <th>QTD</th>
+                <th>PREÇO</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {cart.map((item, index) => (
+                <tr key={index}>
+                  <td><img width="100" src={item.image} alt={item.description} /></td>
+                  <td>{item.description}</td>
+                  <td>{item.quantity}</td>
+                  <td>R$ {item.price.toFixed(2).replace('.', ',')}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+          <div className='checkout'>
+            <button
+              onClick={() => finishOrder()}
+            >FINALIZAR PEDIDO</button>
+            <div>TOTAL: R${
+              cart.reduce((acc, item) => {
+                return acc += item.price * item.quantity
+              }, 0).toFixed(2).replace('.', ',')}
+            </div>
+          </div>
+        </div>
       }
-      <div>
-      <button>FINALIZAR PEDIDO</button>
-      <div>TOTAL: R${
-        cart.reduce((acc, item) => {
-          return acc += item.price * item.quantity
-        }, 0).toFixed(2).replace('.', ',')
-        }</div>
-      </div>
     </div>
   )
 }
