7de97ac1609b79245ccc5c4163299c3004760b3e
Kevin Willian <kevinwillian084@gmail.com>
2023-03-05 23:35:41 -0300

    feat:

--- mobile/src/@types/cart.ts
+++ mobile/src/@types/cart.ts
@@ -10,3 +10,5 @@ export type CartContextData = {
   addProduct: (productId: string) => Promise<void>;
   setCart: (cart: ProductCart[]) => void;
 };
+
+export type SumCartItemsQuantity = { [key: string]: number };
