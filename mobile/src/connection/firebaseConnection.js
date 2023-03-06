86770ad7a024e246bbac86e483e7d03d2d2927f1
jefersongjr <jeferson_gjr@hotmail.com>
2023-03-06 14:20:43 -0300

    .env

--- mobile/src/connection/firebaseConnection.js
+++ mobile/src/connection/firebaseConnection.js
@@ -13,9 +13,9 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyC6w7cziH_fQPlINxfK7EpFPaduj_S5BhU",
-  authDomain: "projetoloja-5a3c6.firebaseapp.com",
-  projectId: "projetoloja-5a3c6",
+  apiKey: REACT_APP_API_KEY,
+  authDomain: REACT_APP_AUTH_DOMAIN,
+  projectId: REACT_APP_PROJECT_ID,
 };
 
 // Initialize Firebase
