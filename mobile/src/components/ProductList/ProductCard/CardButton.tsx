dae1abe5ea8b9334d331f3b552b3b4e7cddf8db9
Kevin Willian <kevinwillian084@gmail.com>
2023-03-05 23:13:13 -0300

    feat: Add product CardButton with styles

---
+++ mobile/src/components/ProductList/ProductCard/CardButton.tsx
@@ -0,0 +1,55 @@
+import { Button, Flex, Text } from 'native-base';
+import { useMemo } from 'react';
+import { Product } from '../../../@types/product';
+import { useCart } from '../../../context/CartProvider';
+import { useLiveStore } from '../../../hooks/useLiveStore';
+
+type CardButtonProps = {
+  product: Product;
+};
+
+export function CardButton({ product: { id } }: CardButtonProps) {
+  const { addProduct, cart } = useCart();
+  const [liveStock] = useLiveStore();
+
+  const sumCartItemsQuantity = useMemo(() => {
+    return cart.reduce((sumAmount, product) => {
+      sumAmount[product.id] = product.quantity;
+      return sumAmount;
+    }, {} as { [key: string]: number });
+  }, [cart, liveStock]);
+
+  return (
+    <Button
+      onPress={() => addProduct(id)}
+      _pressed={{ bg: 'transparent' }}
+      w="full"
+      bg={'transparent'}
+      color="white">
+      <Flex
+        bg={'#f8375d'}
+        direction="row"
+        justifyContent="space-between"
+        overflow={'hidden'}
+        borderRadius={'5px'}
+        w="307px">
+        <Text
+          px={'40px'}
+          py={'10px'}
+          bg={'rgba(0, 0, 0, 0.2);'}
+          color="white"
+          fontWeight="bold">
+          {sumCartItemsQuantity[id] ?? 0}
+        </Text>
+        <Text
+          flex={1}
+          color="white"
+          textAlign="center"
+          alignSelf={'center'}
+          fontWeight="bold">
+          ADICIONAR
+        </Text>
+      </Flex>
+    </Button>
+  );
+}
