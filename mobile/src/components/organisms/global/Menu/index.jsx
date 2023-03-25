ac9cc144ae83ec718ae0511eddbb1db8209ef7c5
joao966 <joaooliveira966@gmail.com>
2023-03-25 00:25:13 -0300

    FIX: ajusting layout header

--- mobile/src/components/organisms/global/Menu/index.jsx
+++ mobile/src/components/organisms/global/Menu/index.jsx
@@ -2,31 +2,35 @@ import { useContext } from 'react';
 import { SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import Context from '../../../../context';
 import { useNavigation } from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 export default function Menu() {
   const { productsSelected } = useContext(Context);
   const navigation = useNavigation();
 
   return (
-    <SafeAreaView
-      style={styles.container}
-    >
-        <TouchableOpacity
-          style={styles.touchabled}
-          onPress={() => navigation.navigate('Home')}
-        >
-          <Text style={styles.text}>COGNYSHOES </Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={styles.touchabled}
-          onPress={() => navigation.navigate('ShoppingCart')}
-        >
-          <Text style={styles.text} >Meu carrinho</Text>
-          <View>
-            <Text style={styles.text}>{productsSelected.length}</Text>
-            <Text style={styles.text}>itens</Text>
-          </View>
-        </TouchableOpacity>
+    <SafeAreaView style={styles.container}>
+      <TouchableOpacity onPress={() => navigation.navigate('Home')}>
+        <Text style={styles.text}>COGNYSHOES </Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => navigation.navigate('ShoppingCart')}>
+        <Icon style={{marginRight: 20}} name="shopping-cart" size={40} color="white" />
+      </TouchableOpacity>
+      <View
+        style={{
+          position: 'absolute',
+          top: -5,
+          right: 5,
+          backgroundColor: 'red',
+          borderRadius: 10,
+          minWidth: 20,
+          height: 20,
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <Text style={{ color: '#fff', fontSize: 12 }}>{productsSelected.length}</Text>
+      </View>
   </SafeAreaView>
   );
 };
@@ -38,15 +42,15 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between',
     color: '#fff',
     marginTop: 36,
-    width: '100%'
+    width: '100%',
   },
   text: {
     color: '#fff',
+    fontSize: 18,
+    marginTop: 12,
+    marginLeft: 8,
   },
-  touchabled: {
-    display: 'flex',
-    justifyContent: 'center',
-    paddingLeft: 16,
-    paddingRight: 16,
+  iconCart: {
+    marginRight: '20px'
   }
 });
