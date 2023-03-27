926dc3afe99db48a5af5dc25a69625a438e846e7
André Santos <x_sode@hotmail.com>
2023-03-27 13:05:52 -0300

    fix: :bug: firebaseconfig file
    fix and changing the gitignore file

--- web/src/utils/firebaseConfig.jsx
+++ web/src/utils/firebaseConfig.jsx
@@ -1,12 +1,12 @@
 // Import the functions you need from the SDKs you need
-import firebase from 'firebase/app';
-import 'firebase/database';
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+// eslint-disable-next-line import/prefer-default-export
+export const firebaseConfig = {
   apiKey: 'AIzaSyAzQ6afC5bXqK79p6N5YcIiYFjeoAI1kDo',
   authDomain: 'cogny-front-end-test-b0e85.firebaseapp.com',
   projectId: 'cogny-front-end-test-b0e85',
@@ -15,9 +15,3 @@ const firebaseConfig = {
   appId: '1:922557791872:web:4374a677f1e3985b1af065',
   measurementId: 'G-F9TSS3DBR8',
 };
-
-// Initialize Firebase
-
-firebase.initializeApp(firebaseConfig);
-
-export default firebase;
